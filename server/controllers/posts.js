291aaf1c7aa5d7931603acc899477d7a991868f9
Rishabh Mediratta <rishabh@Rishabhs-MacBook-Pro-2.local>
2023-04-15 10:30:40 -0500

    w

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -40,7 +40,7 @@ export const getFeedPosts = async (req, res) => {
 export const getUserPosts = async (req, res) => {
     try {
         const { userId } = req.params;
-        const post = await Post.find({ userId });
+        const post = await Post.find({ userId }); // posts with one constraint
         res.status(200).json(post);
     } catch (err) {
         res.status(404).json({ message: err.message });
@@ -55,6 +55,7 @@ export const likePost = async (req, res) => {
         const post = await Post.findById(id);
         const isLiked = post.likes.get(userId);
 
+        // if liked, remove like else like the post
         if (isLiked) {
             post.likes.delete(userId)
         } else {
